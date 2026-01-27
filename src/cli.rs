58af9a7f33be394a64240abbb6043f67354f0972
Monti-27 <msaini27004@gmail.com>
2026-01-27 23:40:49 +0530

    implement list command

--- src/cli.rs
+++ src/cli.rs
@@ -1,5 +1,7 @@
 use std::env;
 
+use crate::storage;
+
 pub fn run() {
     let args: Vec<String> = env::args().collect();
 
@@ -11,24 +13,31 @@ pub fn run() {
     let cmd = args[1].as_str();
 
     match cmd {
-        "list" => {
-            // will add loading and printing todos
-            println!("Listing todos...");
-        }
-        "add" => {
-            // will add and save todos
-            println!("Adding todo...");
-        }
+        "list" => list_todos(),
+        "add" => println!("Adding todo... (next commit)"),
         _ => {
-            // this is the default case
             println!("Unknown command: {cmd}");
             print_help();
         }
     }
 }
 
+fn list_todos() {
+    // load todos from file
+    let todos = storage::load();
+
+    if todos.is_empty() {
+        println!("No todos found :(");
+        return;
+    }
+
+    for t in todos {
+        let status = if t.done { "done" } else { "not done" };
+        println!("[{status}] #{} - {}", t.id, t.text);
+    }
+}
+
 fn print_help() {
-    // keeping the help separate so the main logic stay clean
     println!("Commands:");
     println!("  add \"todo text\"");
     println!("  list");
